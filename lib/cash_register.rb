da1f8648433489e46afcb24d104d9cef43ebac09
Mickae Truman <mtruman@nyit.edu>
2019-03-21 02:30:33 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -26,7 +26,7 @@ end
  #end
 #end
    def apply_discount()
-    if @discount > 0
+    if discount != 0
       self.total = (total * ((100.0 - discount.to_f)/100)).to_i
       "After the discount, the total comes to $#{@total.to_i}."
     else
