3132f6eb8fb174ddeb3fd0708d8e6067dc380f7e
Mickae Truman <mtruman@nyit.edu>
2019-03-21 02:58:26 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -11,6 +11,7 @@ attr_accessor :total, :discount, :quantity, :item, :last_transaction
 def add_item(title, price, quantity = 1)
   self.total += price*quantity
   quantity.times do
+    @items << title
 end
  @last_transaction = price * quantity
  end
