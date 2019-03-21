43a078031632d8b454f0df4a71ec38c6947352f3
Mickae Truman <mtruman@nyit.edu>
2019-03-21 02:34:13 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -25,14 +25,14 @@ return "After the discount, the total comes to $#{total}."
    return "There is no discount to apply."
  end
 end
-   def apply_discount()
-    if discount != 0
-      self.total = (total * ((100.0 - discount.to_f)/100)).to_i
-      "After the discount, the total comes to $#{self.total}."
-    else
-      "There is no discount to apply."
-    end
-  end
+   #def apply_discount()
+    #if discount != 0
+      #self.total = (total * ((100.0 - discount.to_f)/100)).to_i
+      #"After the discount, the total comes to $#{self.total}."
+    #else
+      #"There is no discount to apply."
+    #end
+  #end
 
 def items 
   items = []
